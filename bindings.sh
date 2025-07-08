17a2ffbbd3055154d09162aeb3d28a516f8b7829
Adam Ciarciński <aciarcinski@teonite.com>
2025-07-08 17:47:32 +0200

    Build xcframework

--- bindings.sh
+++ bindings.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 set -e
 
-cargo build --release
-cargo run --release --bin uniffi-bindgen generate \
-    --library target/release/libboringtun.dylib \
-    --language swift \
-    --out-dir ${PWD}/NetExt/BoringTun
-ln -f -s ${PWD}/target/release/libboringtun.dylib ${PWD}/NetExt/BoringTun/libboringtun.dylib
+CARGO="${HOME}/.cargo/bin/cargo"
+
+for TARGET in aarch64-apple-darwin aarch64-apple-ios aarch64-apple-ios-sim
+do
+    echo "${TARGET}"
+    ${CARGO} build --lib --release --target ${TARGET}
+done
+
+# ${CARGO} run --release --bin uniffi-bindgen -- generate \
+#     --crate boringtun \
+#     --library target/release/libboringtun.dylib \
+#     --language swift \
+#     --out-dir target/uniffi
+rm -f -r target/uniffi
+${CARGO} run --release --bin uniffi-bindgen -- \
+    --xcframework --headers --modulemap --swift-sources \
+    target/release/libboringtun.dylib target/uniffi
+mv target/uniffi/boringtun.swift ${PWD}/NetExt/BoringTun/
+rm -f -r ${PWD}/NetExt/BoringTun/boringtun.xcframework
+xcrun xcodebuild -create-xcframework \
+    -library target/aarch64-apple-darwin/release/libboringtun.dylib -headers target/uniffi \
+    -library target/aarch64-apple-ios/release/libboringtun.dylib -headers target/uniffi \
+    -library target/aarch64-apple-ios-sim/release/libboringtun.dylib -headers target/uniffi \
+    -output ${PWD}/NetExt/BoringTun/boringtun.xcframework
+mv target/uniffi/boringtunFFI.h ${PWD}/NetExt/BoringTun/
--- boringtun/src/serialization.rs
+++ boringtun/src/serialization.rs
@@ -4,8 +4,10 @@ use aead::OsRng;
 use base64::prelude::*;
 use x25519_dalek::StaticSecret;
 
+const KEY_SIZE: usize = 32;
+
 #[derive(uniffi::Object)]
-pub struct KeyBytes(pub(crate) [u8; 32]);
+pub struct KeyBytes(pub(crate) [u8; KEY_SIZE]);
 
 #[derive(Debug, uniffi::Enum)]
 pub enum KeyBytesError {
@@ -31,12 +33,12 @@ impl FromStr for KeyBytes {
 
     /// Can parse a secret key from a hex or base64 encoded string.
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut internal = [0u8; 32];
+        let mut internal = [0u8; KEY_SIZE];
 
         match s.len() {
             64 => {
                 // Try to parse as hex
-                for i in 0..32 {
+                for i in 0..KEY_SIZE {
                     internal[i] = u8::from_str_radix(&s[i * 2..=i * 2 + 1], 16)
                         .map_err(|_| KeyBytesError::IllegalCharacter)?;
                 }
@@ -72,13 +74,35 @@ impl KeyBytes {
         self.0.into()
     }
 
-    /// Provide base64-encoded public key.
-    pub fn public_key(&self) -> String {
-        BASE64_STANDARD.encode(self.0)
-    }
-
     #[uniffi::constructor]
     pub fn from_string(s: &str) -> Result<Self, KeyBytesError> {
         Self::from_str(s)
+    }
+
+    #[uniffi::constructor]
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self, KeyBytesError> {
+        let internal = bytes.try_into().map_err(|_| KeyBytesError::IllegalSize)?;
+        Ok(Self(internal))
+    }
+
+    #[must_use]
+    pub fn to_base64(&self) -> String {
+        BASE64_STANDARD.encode(self.0)
+    }
+
+    #[must_use]
+    pub fn to_lower_hex(&self) -> String {
+        let mut hex = String::with_capacity(64);
+        let to_char = |nibble: u8| -> char {
+            (match nibble {
+                0..=9 => b'0' + nibble,
+                _ => nibble + b'a' - 10,
+            }) as char
+        };
+        self.0.iter().for_each(|byte| {
+            hex.push(to_char(*byte >> 4));
+            hex.push(to_char(*byte & 0xf));
+        });
+        hex
     }
 }
