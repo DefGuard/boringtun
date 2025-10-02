b085f37a995efa7f2db1f64c331c5b3759ef280d
Adam Ciarciński <adam@defguard.net>
2025-10-02 11:43:31 +0200

    Removed unused comments

--- bindings.sh
+++ bindings.sh
@@ -9,28 +9,10 @@ do
     ${CARGO} build --lib --release --target ${TARGET}
 done
 
-# ${CARGO} build --lib --release
-# ${CARGO} run --release --bin uniffi-bindgen -- generate \
-#     --crate boringtun \
-#     --library target/release/libboringtun.dylib \
-#     --language swift \
-#     --out-dir target/uniffi
-
 rm -f -r target/uniffi
 ${CARGO} run --release --bin uniffi-bindgen -- \
     --xcframework --headers --modulemap --swift-sources \
     target/aarch64-apple-darwin/release/libboringtun.a target/uniffi
-
-# swiftc \
-#     -module-name boringtun \
-#     -emit-library -o libboringtun.dylib \
-#     -emit-module -emit-module-path ${PWD}/NetExt/BoringTun/ \
-#     -parse-as-library \
-#     -I target/uniffi/ \
-#     -L target/release/ \
-#     -lboringtun \
-#     -Xcc -fmodule-map-file=target/uniffi/boringtunFFI.modulemap \
-#     target/uniffi/boringtun.swift
 
 mv target/uniffi/boringtun.swift ${PWD}/../VPNExtension/BoringTun/
 rm -f -r ${PWD}/../VPNExtension/BoringTun/boringtun.xcframework
