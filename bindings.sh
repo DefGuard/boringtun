df20f088a306b449786441a911da69621b8ee2fe
Adam Ciarciński <adam@defguard.net>
2026-02-13 11:24:15 +0100

    Better bindings.sh

--- bindings.sh
+++ bindings.sh
@@ -2,6 +2,7 @@
 set -e
 
 CARGO="${HOME}/.cargo/bin/cargo"
+DST="${PWD}/../VPNExtension/BoringTun"
 
 for TARGET in aarch64-apple-darwin aarch64-apple-ios aarch64-apple-ios-sim
 do
@@ -14,11 +15,12 @@ ${CARGO} run --release --bin uniffi-bindgen -- \
     --xcframework --headers --modulemap --swift-sources \
     target/aarch64-apple-darwin/release/libdefguard_boringtun.a target/uniffi
 
-mv target/uniffi/defguard_boringtun.swift ${PWD}/../VPNExtension/BoringTun/
-rm -f -r ${PWD}/../VPNExtension/BoringTun/defguard_boringtun.xcframework
+mkdir -p "${DST}"
+mv target/uniffi/defguard_boringtun.swift ${DST}/
+rm -f -r ${DST}/defguard_boringtun.xcframework
 xcrun xcodebuild -create-xcframework \
     -library target/aarch64-apple-darwin/release/libdefguard_boringtun.a -headers target/uniffi \
     -library target/aarch64-apple-ios/release/libdefguard_boringtun.a -headers target/uniffi \
     -library target/aarch64-apple-ios-sim/release/libdefguard_boringtun.a -headers target/uniffi \
-    -output ${PWD}/../VPNExtension/BoringTun/defguard_boringtun.xcframework
-mv target/uniffi/defguard_boringtunFFI.h ${PWD}/../VPNExtension/BoringTun/
+    -output ${DST}/defguard_boringtun.xcframework
+mv target/uniffi/defguard_boringtunFFI.h ${DST}/
